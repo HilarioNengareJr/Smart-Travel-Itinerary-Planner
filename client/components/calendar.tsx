7e2004eba7915753b534e1dfdc3c4a9a177a4aa6
Hilario Nengare <hnengare@gmail.com>
2024-08-16 14:35:39 +0300

    added buttons for calendar

--- client/components/calendar.tsx
+++ client/components/calendar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Calendar: React.FC = () => {
     return (
-        <div className='pt-40 px-24 grid grid-rows-2 gap-4 relative'>
+        <div className='pt-40 px-44 grid grid-rows-2 gap-4 relative'>
            <div className='flex flex-col'>
            <span className='pb-4 text-2xl font-bold'>
                 When do you want to go?
@@ -11,6 +11,14 @@ const Calendar: React.FC = () => {
             <span className='font-thin'>
                 Choose a date range or length of stay, up to 7 days.
             </span>
+           </div>
+           <div className='bg-gray-300 rounded-full flex flex-row justify-center h-12 p-1'>
+            <button className='w-1/2 focus:bg-white hover:bg-gray-400 rounded-full'>
+                Dates (MM/DD)
+            </button>
+            <button className='w-1/2 focus:bg-white hover:bg-gray-400 rounded-full'>
+                Trip Length
+                </button>
            </div>
         </div>
     );
